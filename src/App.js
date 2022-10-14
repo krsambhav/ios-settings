590ce5bc9386f3c272fbd193f7802769a923d3e4
Kumar Sambhav <61407549+divyank-dagur@users.noreply.github.com>
2022-10-15 00:46:21 +0530

    Fixed Overflow

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {FaIcons} from 'react-icons/fa';
 function App() {
   return (
     <div className="App w-screen h-screen flex flex-row justify-center items-center">
-      <div className="screen w-[300px] h-[550px] shadow-2xl bg-[#f2f1f6]">
+      <div className="screen w-[300px] shadow-2xl bg-[#f2f1f6]">
         <div className="settings-title w-[100%] mt-8 ml-5 text-xl">
           Settings
         </div>
