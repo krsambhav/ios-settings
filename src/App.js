7f7608b9be4c64a97fe3df292e9c2c34c8146eb0
Kumar Sambhav <66507187+Kapeesh-Upadhyay@users.noreply.github.com>
2022-10-14 20:48:16 +0530

    Added VPN

--- src/App.js
+++ src/App.js
@@ -78,6 +78,14 @@ function App() {
                 <div className="left">Personal Hotspot</div>
               </div>
             </div>
+            <div className="menu flex flex-row gap-2 items-center w-[100%]">
+              <div className="icon">
+                <RiHotspotFill />
+              </div>
+              <div className="text flex flex-row gap-[140px]">
+                <div className="left">VPN</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
