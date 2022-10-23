657f0298ef7c4d856cd57b4665b83b897ddbcb90
Kumar Sambhav <116516936+Kumar-The-Goat@users.noreply.github.com>
2022-10-24 00:20:59 +0530

    Added Music

--- src/App.js
+++ src/App.js
@@ -321,6 +321,12 @@ function App() {
               </div>
               <div className="text">Health</div>
             </div>
+            <div className="menu flex flex-row gap-2 items-center w-[100%]">
+              <div className="icon">
+                <IoMdHeart />
+              </div>
+              <div className="text">Music</div>
+            </div>
           </div>
         </div>
       </div>
