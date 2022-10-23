203a3e1fca41ba2304eeefad119c19697fe64bde
Kumar Sambhav <116516524+King-Upadhyay@users.noreply.github.com>
2022-10-24 00:07:50 +0530

    Added Maps

--- src/App.js
+++ src/App.js
@@ -290,6 +290,12 @@ function App() {
               </div>
               <div className="text">Translate</div>
             </div>
+            <div className="menu flex flex-row gap-2 items-center w-[100%]">
+              <div className="icon">
+                <MdTranslate />
+              </div>
+              <div className="text">Maps</div>
+            </div>
           </div>
         </div>
       </div>
