713653001c6d764ad6f695a0471d31638d66aba9
Kumar Sambhav <116516725+Sir-Kumar@users.noreply.github.com>
2022-10-24 00:14:28 +0530

    Added Shortcuts

--- src/App.js
+++ src/App.js
@@ -309,6 +309,12 @@ function App() {
               </div>
               <div className="text">Measure</div>
             </div>
+            <div className="menu flex flex-row gap-2 items-center w-[100%]">
+              <div className="icon">
+                <MdLinearScale />
+              </div>
+              <div className="text">Shortcuts</div>
+            </div>
           </div>
         </div>
       </div>
